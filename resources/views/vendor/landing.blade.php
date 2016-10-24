f027f825a034704cffd16791ebd9e4c21230c7c3
Jessica Sattler <jessicasattler28@gmail.com>
2016-10-24 12:29:07 -0500

    Updated the questions in the landing page and created a landing.css page

--- resources/views/vendor/landing.blade.php
+++ resources/views/vendor/landing.blade.php
@@ -4,7 +4,7 @@
 
 {{-- the following section may change if I user custom css --}}
 @section('css')
-	<link rel="stylesheet" type="text/css" href="/assets/css/register.css">
+	<link rel="stylesheet" type="text/css" href="/assets/css/landing.css">
 	<link rel="stylesheet" type="text/css" href="/assets/css/form-elements.css">
 @stop
 
@@ -33,6 +33,60 @@
 						</div>
 						<div class="col-sm-7 text-left">
 							<h4>What is your price range?</h4>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">$</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">$$</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">$$$</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value"">$$$$</label>
+							</div>
+						</div>
+						<div class="col-sm-7 text-left">
+							<h4>What cuisine are you craving?</h4>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">American</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Breakfast Food</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Cajun</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value"">Chinese</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value"">German</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value"">Indian</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value"">Italian</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value"">Japanese/Sushi</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value"">Mediterranean</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value"">Mexican</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value"">Soul</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value"">Thai</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value"">Vietnamese</label>
+							</div>
 							
 						</div>
 					</div>
