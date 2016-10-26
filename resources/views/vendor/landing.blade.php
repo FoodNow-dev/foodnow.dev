4841cc548b6cab76a5d41498fb031326ab07ee4c
Jessica Sattler <jessicasattler28@gmail.com>
2016-10-25 22:03:39 -0500

    Landing blade now is now more mobile responsive

--- resources/views/vendor/landing.blade.php
+++ resources/views/vendor/landing.blade.php
@@ -20,8 +20,8 @@
 						<h1><strong>My Restaurant Preferences:</strong></h1>
 						<form class="form-horizontal" method="POST" action="#">
 							<div class="form-group">
-								<label for="distance" class="col-sm-6 control-label">How far are you willing to go?</label>
-								<div class="col-sm-6">
+								<label for="distance" class="control-label col-sm-6">How far are you willing to go?</label>
+								<div class="col-sm-6 col-xs-6 col-xs-offset-3 col-sm-offset-0">
      								<select name="distance" class="form-control">
 										 <option value="5">5 miles</option>
 										 <option value="10">10 miles</option>
@@ -32,7 +32,7 @@
     						</div>
     						<div class="form-group">
     							<label for="price" class="col-sm-6 control-label">What is your price range?</label>
-    							<div class="col-sm-6">
+    							<div class="col-sm-6 col-xs-6 col-xs-offset-3 col-sm-offset-0">
     								<select name="price" class="form-control">
     									<option value="1">$</option>
     									<option value="2">$$</option>
@@ -43,7 +43,7 @@
  							</div>
  							<div class="form-group">
  								<label for="cuisine" class="col-sm-6 control-label">What cuisine type are you craving</label>
- 								<div class="col-sm-6">
+ 								<div class="col-sm-6 col-xs-6 col-xs-offset-3 col-sm-offset-0">
  									<select name="price" class="form-control">
 
 	 									<option value="american">American</option>
@@ -63,14 +63,14 @@
  									</select>
  								</div>
  							</div>
- 							<div class="col-sm-offset-4 col-sm-6 col-xs-2" ><button type="submit" class="btn btn-default">Submit</button></div>
+ 							<div class="col-xs-offset-2 col-xs-6" ><button type="submit" class="btn btn-default">Submit</button></div>
 						</form>
 					</div>
 					<div class="col-sm-5 col-sm-offset-1 text">
 						<h1>OR....</h1>
 						<h1> Let Us Choose Your Dinner Tonight!</h1>
 						<br>
-						<div class="col-sm-6 col-sm-offset-3 col-xs-2" ><button type="submit" class="btn btn-success">Pick at Random</button></div>
+						<div class="col-xs-6 col-xs-offset-3" ><button type="submit" class="btn btn-default">Pick at Random</button></div>
 					</div>
 				</div>
 			</div>
