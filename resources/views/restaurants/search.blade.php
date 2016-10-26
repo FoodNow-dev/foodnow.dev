3382879d645f590b434b4e6a59363ba2dc83ac26
Jessica Sattler <jessicasattler28@gmail.com>
2016-10-26 10:48:21 -0500

    Edit blade is in the right place (restaurants folder)

--- resources/views/restaurants/search.blade.php
+++ resources/views/restaurants/search.blade.php
@@ -20,8 +20,8 @@
 						<h1><strong>My Restaurant Preferences:</strong></h1>
 						<form class="form-horizontal" method="POST" action="#">
 							<div class="form-group">
-								<label for="distance" class="col-sm-6 control-label">How far are you willing to go?</label>
-								<div class="col-sm-6">
+								<label for="distance" class="control-label col-xs-12 col-sm-6 text-left">How far are you willing to go?</label>
+								<div class="col-sm-6 col-xs-6 col-xs-offset-3 col-sm-offset-0">
      								<select name="distance" class="form-control">
 										 <option value="5">5 miles</option>
 										 <option value="10">10 miles</option>
@@ -31,8 +31,8 @@
     							</div>
     						</div>
     						<div class="form-group">
-    							<label for="price" class="col-sm-6 control-label">What is your price range?</label>
-    							<div class="col-sm-6">
+    							<label for="price" class="col-xs-12 col-sm-6 control-label text-left">What is your price range?</label>
+    							<div class="col-sm-6 col-xs-6 col-xs-offset-3 col-sm-offset-0">
     								<select name="price" class="form-control">
     									<option value="1">$</option>
     									<option value="2">$$</option>
@@ -42,8 +42,8 @@
     							</div>
  							</div>
  							<div class="form-group">
- 								<label for="cuisine" class="col-sm-6 control-label">What cuisine type are you craving</label>
- 								<div class="col-sm-6">
+ 								<label for="cuisine" class="col-xs-12 col-sm-6 control-label text-left">What cuisine type are you craving</label>
+ 								<div class="col-sm-6 col-xs-6 col-xs-offset-3 col-sm-offset-0">
  									<select name="price" class="form-control">
 
 	 									<option value="american">American</option>
@@ -63,20 +63,17 @@
  									</select>
  								</div>
  							</div>
- 							<div class="col-sm-offset-4 col-sm-6 col-xs-2" ><button type="submit" class="btn btn-default">Submit</button></div>
+ 							<div class="col-xs-offset-2 col-xs-6" ><button type="submit" class="btn btn-default">Submit</button></div>
 						</form>
 					</div>
 					<div class="col-sm-5 col-sm-offset-1 text">
-						{{-- <h1>OR....</h1>
+						<h1>OR....</h1>
 						<h1> Let Us Choose Your Dinner Tonight!</h1>
 						<br>
-
-						<div class="col-sm-6 col-sm-offset-3 col-xs-2" ><button type="submit" class="btn btn-default">Pick at Random</button></div> --}}
-
-
-
-						<div class="col-sm-6 col-sm-offset-3 col-xs-2" ><button type="submit" class="btn btn-success btn-lg">Let Us Choose Your Dinner Tonight!</button></div>
-						<div class="col-sm-6 col-sm-offset-3 col-xs-2" ><button type="submit" class="btn btn-success">Pick at Random</button></div>
+						<div class="col-xs-6 col-xs-offset-3" >
+						    <button type="submit" class="btn btn-default">Pick at Random
+						    </button>
+						</div>
 					</div>
 				</div>
 			</div>
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -21,112 +21,127 @@
 						</div>
 						<form class="form-horizontal" method="POST" action="#">
 							<div class = "row">
-							<div class="form-group col-xs-7 col-sm-10">
-								<input type="text" name="firstName" class="form-control " placeholder="First name" value="">
-							</div>
-							</div>
-
-							<div class = "row">
-							<div class="form-group col-xs-7 col-sm-10">
-								<input type="text" name="lastName" class="form-control" placeholder="Last name" value="">
-							</div>
+								<div class="form-group col-xs-7 col-sm-10">
+									<input type="text" name="firstName" class="form-control " placeholder="First name" value="">
+								</div>
 							</div>
 
 							<div class = "row">
-							<div class="form-group col-xs-7 col-sm-10">
-								<input type="text" name="phone" class="form-control" placeholder="Phone #"
-								value="">
-							</div>
-							</div>
-
-							<div class = "row">
-							<div class="form-group col-xs-10 col-sm-10">
-								<select name="form-state" class="form-state form-control" id="form-state">
-										<option>State ...</option>
-										<option value="AK">Alaska</option>
-										<option value="AL">Alabama</option>
-										<option value="AR">Arkansas</option>
-										<option value="AZ">Arizona</option>
-										<option value="CA">California</option>
-										<option value="CO">Colorado</option>
-										<option value="CT">Connecticut</option>
-										<option value="DC">District of Columbia</option>
-										<option value="DE">Delaware</option>
-										<option value="FL">Florida</option>
-										<option value="GA">Georgia</option>
-										<option value="HI">Hawaii</option>
-										<option value="IA">Iowa</option>
-										<option value="ID">Idaho</option>
-										<option value="IL">Illinois</option>
-										<option value="IN">Indiana</option>
-										<option value="KS">Kansas</option>
-										<option value="KY">Kentucky</option>
-										<option value="LA">Louisiana</option>
-										<option value="MA">Massachusetts</option>
-										<option value="MD">Maryland</option>
-										<option value="ME">Maine</option>
-										<option value="MI">Michigan</option>
-										<option value="MN">Minnesota</option>
-										<option value="MO">Missouri</option>
-										<option value="MS">Mississippi</option>
-										<option value="MT">Montana</option>
-										<option value="NC">North Carolina</option>
-										<option value="ND">North Dakota</option>
-										<option value="NE">Nebraska</option>
-										<option value="NH">New Hampshire</option>
-										<option value="NJ">New Jersey</option>
-										<option value="NM">New Mexico</option>
-										<option value="NV">Nevada</option>
-										<option value="NY">New York</option>
-										<option value="OH">Ohio</option>
-										<option value="OK">Oklahoma</option>
-										<option value="OR">Oregon</option>
-										<option value="PA">Pennsylvania</option>
-										<option value="PR">Puerto Rico</option>
-										<option value="RI">Rhode Island</option>
-										<option value="SC">South Carolina</option>
-										<option value="SD">South Dakota</option>
-										<option value="TN">Tennessee</option>
-										<option value="TX">Texas</option>
-										<option value="UT">Utah</option>
-										<option value="VA">Virginia</option>
-										<option value="VT">Vermont</option>
-										<option value="WA">Washington</option>
-										<option value="WI">Wisconsin</option>
-										<option value="WV">West Virginia</option>
-										<option value="WY">Wyoming</option>
-								</select>
-							</div>
+								<div class="form-group col-xs-7 col-sm-10">
+									<input type="text" name="lastName" class="form-control" placeholder="Last name" value="">
+								</div>
 							</div>
 
 							<div class = "row">
-							<div class="form-group col-xs-7 col-sm-10">
-								<input type="text" name="city" class="form-control" placeholder="City"
-								value="">
-							</div>
-							</div>
-
-							<div class = "row">
-							<div class="form-group col-xs-7 col-sm-10 col-sm-offset-4">
-								<input type="text" name="zipcode" class="form-control" placeholder="Zipcode"
-								value="">
-							</div>
-							</div>
-
-							{{-- <label for="password" class="col-xs-6 col-lg-5 control-label">Password:</label> --}}
-							<div class = "row">
-							<div class="form-group col-xs-offset-3 col-sm-offset-4 col-sm-6 col-lg-7">
-								<button type="submit" class="btn btn-success">Change Password</button>
-							</div>
+								<div class="form-group col-xs-7 col-sm-10">
+									<input type="text" name="phone" class="form-control" placeholder="Phone #"
+									value="">
+								</div>
 							</div>
 
 							<div class = "row">
-							<div class="form-group col-xs-offset-3 col-sm-2 col-lg-7">
-								<button type="submit" class="btn btn-success">Submit</button>
-
+								<div class="form-group col-xs-10 col-sm-10">
+									<select name="form-state" class="form-state form-control" id="form-state">
+											<option>State ...</option>
+											<option value="AK">Alaska</option>
+											<option value="AL">Alabama</option>
+											<option value="AR">Arkansas</option>
+											<option value="AZ">Arizona</option>
+											<option value="CA">California</option>
+											<option value="CO">Colorado</option>
+											<option value="CT">Connecticut</option>
+											<option value="DC">District of Columbia</option>
+											<option value="DE">Delaware</option>
+											<option value="FL">Florida</option>
+											<option value="GA">Georgia</option>
+											<option value="HI">Hawaii</option>
+											<option value="IA">Iowa</option>
+											<option value="ID">Idaho</option>
+											<option value="IL">Illinois</option>
+											<option value="IN">Indiana</option>
+											<option value="KS">Kansas</option>
+											<option value="KY">Kentucky</option>
+											<option value="LA">Louisiana</option>
+											<option value="MA">Massachusetts</option>
+											<option value="MD">Maryland</option>
+											<option value="ME">Maine</option>
+											<option value="MI">Michigan</option>
+											<option value="MN">Minnesota</option>
+											<option value="MO">Missouri</option>
+											<option value="MS">Mississippi</option>
+											<option value="MT">Montana</option>
+											<option value="NC">North Carolina</option>
+											<option value="ND">North Dakota</option>
+											<option value="NE">Nebraska</option>
+											<option value="NH">New Hampshire</option>
+											<option value="NJ">New Jersey</option>
+											<option value="NM">New Mexico</option>
+											<option value="NV">Nevada</option>
+											<option value="NY">New York</option>
+											<option value="OH">Ohio</option>
+											<option value="OK">Oklahoma</option>
+											<option value="OR">Oregon</option>
+											<option value="PA">Pennsylvania</option>
+											<option value="PR">Puerto Rico</option>
+											<option value="RI">Rhode Island</option>
+											<option value="SC">South Carolina</option>
+											<option value="SD">South Dakota</option>
+											<option value="TN">Tennessee</option>
+											<option value="TX">Texas</option>
+											<option value="UT">Utah</option>
+											<option value="VA">Virginia</option>
+											<option value="VT">Vermont</option>
+											<option value="WA">Washington</option>
+											<option value="WI">Wisconsin</option>
+											<option value="WV">West Virginia</option>
+											<option value="WY">Wyoming</option>
+									</select>
+								</div>
 							</div>
+
+							<div class = "row">
+								<div class="form-group col-xs-7 col-sm-10">
+									<input type="text" name="city" class="form-control" placeholder="City"
+									value="">
+								</div>
 							</div>
 
+							<div class = "row">
+								<div class="form-group col-xs-7 col-sm-10 col-sm-offset-4">
+									<input type="text" name="zipcode" class="form-control" placeholder="Zipcode"
+									value="">
+								</div>
+							</div>
+
+		
+							<div class = "row">
+								<div class="form-group col-xs-offset-3 col-sm-offset-4 col-sm-6 col-lg-11">
+									<button type="submit" class="btn btn-success">Change Password</button>
+								</div>
+							</div>
+
+							<div class = "row">
+								<div class="form-group col-xs-offset-3 col-sm-2 col-lg-11">
+									<button type="submit" class="btn btn-success">Submit</button>
+
+								</div>
+							</div>
+						</form>
+					</div>
+					<div col-xs-2 col-xs-offset-2 text>
+						<form>
+							<div class="col-sm-12 col-sm-offset-12 text">
+								<div class="row>"
+									<div class="form-group">
+	                        			<label for="image">Item image</label>
+	                        			<input type="file" class="form-control-file" id="image_url" name="image_url" enctype="multipart/form-data"  aria-describedby="fileHelp">
+	                       				 <small id="fileHelp" class="form-text text-muted">Add a Profile Picture. Allowed file types are .jpeg, .jpg, .gif, and .png</small>
+	                    			</div>
+	                    			<div class="col-sm-7 col-sm-offset-2">
+	                           			 <button type="submit" class="btn btn-primary">Submit</button>
+	                       		    </div>
+	                       		</div>
+							</div>
 						</form>
 					</div>
 				</div>
