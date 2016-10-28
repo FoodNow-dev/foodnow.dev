99d8e20e6dbaf49f8c5ceab8298c0726bf6b8b01
Mittsy Tidwell <mtidwell506@gmail.com>
2016-10-28 18:06:56 -0500

    another test

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -18,6 +18,8 @@ function showPosition(position) {
     var latlon = position.coords.latitude + ", " + position.coords.longitude;
 
     console.log(latlon);
+    console.log(position);
+    console.log("TESTING");
     // var img_url = "https://maps.googleapis.com/maps/api/staticmap?center=" + latlon + "&zoom=14&size=400x300&sensor=false";
 
     // document.getElementById("map").innerHTML = "<img src='" + img_url + "'>";
