8bc1da352f5232471801b7e652f1729211ece9a0
Mittsy Tidwell <mtidwell506@gmail.com>
2016-10-29 15:36:47 -0500

    blue pin

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -49,7 +49,7 @@ function initMap(lat, lon) {
         map: map,
         icon: {
             url: '/assets/img/blue.png',
-            scaledSize: new google.maps.Size(20, 20)
+            scaledSize: new google.maps.Size(20, 30)
         }
     });
 
