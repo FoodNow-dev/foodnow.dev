f32772e8bbc975546a49c8c772cbb756341e1593
Jessica Sattler <jessicasattler28@gmail.com>
2016-10-29 19:49:45 -0500

    I can now add or remove input fields

--- resources/views/restaurants/restaurant.blade.php
+++ resources/views/restaurants/restaurant.blade.php
@@ -69,6 +69,12 @@
 						<form class="form-horizontal col-xs-10 col-xs-offset-3" method="POST" action="/restaurants/restaurant">
 						{!! csrf_field() !!}
 							<div class="form-group col-xs-8 col-xs-offset-7">
+							{{-- dynamic buttons --}}
+							<div class="input_fields_wrap">
+   								 <button class="btn btn-primary add_field_button">Add More Fields</button>
+    						<div>
+    								<input type="text" name="mytext[]">
+    							</div>
 								<input type="text" name="friendName1" class="form-control" placeholder="Friend's name" value="">
 							</div>
 							<div class="form-group col-xs-8 col-xs-offset-7">
@@ -83,12 +89,7 @@
 							<div class="form-group col-xs-8 col-xs-offset-7">
 								<textarea id="email_body" name="email_body" rows="3" cols="25" placeholder="We're meeting in Olive Garden at 7 pm"></textarea>
 							</div>
-							{{-- dynamic buttons --}}
-							<div class="input_fields_wrap">
-    <button class="add_field_button">Add More Fields</button>
-    <div><input type="text" name="mytext[]"></div>
-</div>
-
+							</div>
 							<div class="col-xs-6">
 								<button type="submit" class="btn btn-primary">Send Message</button>
 							</div>
@@ -123,8 +124,27 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> -->
-    <script src="../../../public../assets/js/jQuery.js"></script>
+    {{-- <script src="../../../public../assets/js/jQuery.js"></script> --}}
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="/js/bootstrap.min.js"></script>
-    <script src="/js/story.js"></script>
+    {{-- <script src="../../../public/assets/js/bootstrap.min.js"></script> --}}
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"></script>
+    <script> $(document).ready(function() {
+    var max_fields      = 10; //maximum input boxes allowed
+    var wrapper         = $(".input_fields_wrap"); //Fields wrapper
+    var add_button      = $(".add_field_button"); //Add button ID
+    
+    var x = 1; //initlal text box count
+    $(add_button).click(function(e){ //on add input button click
+        e.preventDefault();
+        if(x < max_fields){ //max input box allowed
+            x++; //text box increment
+            $(wrapper).append('<div><input type="text" name="mytext[]"/><a href="#" class="remove_field">Remove</a></div>'); //add input box
+        }
+    });
+    
+    $(wrapper).on("click",".remove_field", function(e){ //user click on remove text
+        e.preventDefault(); $(this).parent('div').remove(); x--;
+    })
+});</script>
 @stop
