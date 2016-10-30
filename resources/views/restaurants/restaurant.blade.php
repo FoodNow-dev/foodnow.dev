9093f20295e4da6add3608b96fcf6d91121fa52d
Jessica Sattler <jessicasattler28@gmail.com>
2016-10-29 20:44:15 -0500

    When I add an input field to add another friend's phone number, the input field is added above the message input box and not after it

--- resources/views/restaurants/restaurant.blade.php
+++ resources/views/restaurants/restaurant.blade.php
@@ -26,92 +26,44 @@
 				<div class="row">
 					<div class="col-xs-12 col-xs-offset-1 col-sm-5 col-sm-offset-0 text">
 						<h1>Update Your Friends Where Dinner Is</h1>
-						{{-- <form class="form-horizontal col-xs-10 col-xs-offset-3" method="POST" action="/restaurants/restaurant">
-						{!! csrf_field() !!}
-							<div class="form-group col-xs-8 col-xs-offset-7">
-								<input type="text" name="friendName1" class="form-control" placeholder="Friend's name" value="">
-							</div>
-							<div class="form-group col-xs-8 col-xs-offset-7">
-								<input type="text" name="friendPhone1" class="form-control" placeholder="Friend's phone #" value="">
-							</div>
-							<div class="form-group col-xs-8 col-xs-offset-7">
-								<input type="text" name="friendName2" class="form-control" placeholder="Friend's name" value="">
-							</div>
-							<div class="form-group col-xs-8 col-xs-offset-7">
-								<input type="text" id = "friendPhone2" name="friendPhone2" class="form-control" placeholder="Friend's phone #" value="">
-							</div>
-							<div class="form-group col-xs-8 col-xs-offset-7">
-								<textarea id="email_body" name="email_body" rows="3" cols="25" placeholder="We're meeting in Olive Garden at 7 pm"></textarea>
-							</div>
+							<form class="form-horizontal col-xs-10 col-xs-offset-3" method="POST" action="/restaurants/restaurant">
+							{!! csrf_field() !!}
+								<div class="form-group col-xs-8 col-xs-offset-7">
+									{{-- dynamic buttons --}}
+									<div class="input_fields_wrap">
+		   								 <button class="btn btn-primary add_field_button">Invite More Friends</button>
+			    						<div>
+			    							<input type="text" name="mytext[]" placeholder="Phone Number">
+			    						</div>
+			    					</div>
 
-							<div class="col-xs-6">
-								<button type="submit" class="btn btn-primary">Send Message</button>
-							</div>
-							<div class="col-xs-7">
-								@if(session()->has('SUCCESS_MESSAGE'))
-								<div class="alert alert-success">
-									<p>{{session('SUCCESS_MESSAGE') }}</p>
-								</div>
-								@endif
-		
-								@if(session()->has('ERROR_MESSAGE'))
-								<div class="alert alert-danger">
-									<p>{{session('ERROR_MESSAGE') }}</p>
-								</div>
-								@endif
-							</div>
+									
+									<div class="form-group col-xs-8 col-xs-offset-7">
+										<textarea id="email_body" name="email_body" rows="3" cols="25" placeholder="We're meeting in Olive Garden at 7 pm"></textarea>
+									</div>
+									</div>
+									<div class="col-xs-6">
+										<button type="submit" class="btn btn-primary">Send Message</button>
+									</div>
+									<div class="col-xs-10">
+										@if(session()->has('SUCCESS_MESSAGE'))
+										<div class="alert alert-success">
+											<p>{{session('SUCCESS_MESSAGE') }}</p>
+										</div>
+										@endif
+				
+										@if(session()->has('ERROR_MESSAGE'))
+										<div class="alert alert-danger">
+											<p>{{session('ERROR_MESSAGE') }}</p>
+										</div>
+										@endif
+									</div>
 
-							<div class="container">  
-						   		 @yield('content')
-						  </div>
-
-						</form> --}}
-						<form class="form-horizontal col-xs-10 col-xs-offset-3" method="POST" action="/restaurants/restaurant">
-						{!! csrf_field() !!}
-							<div class="form-group col-xs-8 col-xs-offset-7">
-								{{-- dynamic buttons --}}
-							<div class="input_fields_wrap">
-   								 <button class="btn btn-primary add_field_button">Add More Fields</button>
-    						<div>
-    							<input type="text" name="mytext[]">
-    						</div>
-								<input type="text" name="friendName1" class="form-control" placeholder="Friend's name" value="">
-							</div>
-							<div class="form-group col-xs-8 col-xs-offset-7">
-								<input type="text" name="friendPhone1" class="form-control" placeholder="Friend's phone #" value="">
-							</div>
-							<div class="form-group col-xs-8 col-xs-offset-7">
-								<input type="text" name="friendName2" class="form-control" placeholder="Friend's name" value="">
-							</div>
-							<div class="form-group col-xs-8 col-xs-offset-7">
-								<input type="text" id = "friendPhone2" name="friendPhone2" class="form-control" placeholder="Friend's phone #" value="">
-							</div>
-							<div class="form-group col-xs-8 col-xs-offset-7">
-								<textarea id="email_body" name="email_body" rows="3" cols="25" placeholder="We're meeting in Olive Garden at 7 pm"></textarea>
-							</div>
-							</div>
-							<div class="col-xs-6">
-								<button type="submit" class="btn btn-primary">Send Message</button>
-							</div>
-							<div class="col-xs-7">
-								@if(session()->has('SUCCESS_MESSAGE'))
-								<div class="alert alert-success">
-									<p>{{session('SUCCESS_MESSAGE') }}</p>
-								</div>
-								@endif
-		
-								@if(session()->has('ERROR_MESSAGE'))
-								<div class="alert alert-danger">
-									<p>{{session('ERROR_MESSAGE') }}</p>
-								</div>
-								@endif
-							</div>
-
-							<div class="container">  
-						   		 @yield('content')
-						  </div>
-
-						</form>
+									<div class="container">  
+								   		 @yield('content')
+								   </div>
+							</form>
+						</div>
 					</div>
 					<div class="col-xs-12 col-xs-offset-1 col-sm-5 col-sm-offset-1 text">
 						<h1>Reviews</h1>
@@ -139,7 +91,7 @@
         e.preventDefault();
         if(x < max_fields){ //max input box allowed
             x++; //text box increment
-            $(wrapper).append('<div><input type="text" name="mytext[]"/><a href="#" class="remove_field">Remove</a></div>'); //add input box
+            $(wrapper).append('<div><input type="text" name="mytext[]" placeholder="Phone Number"/><a href="#" class="remove_field">Remove</a></div>'); //add input box
         }
     });
     
