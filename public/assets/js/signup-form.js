15e73e68c2401e6a79f79c5bed8dd656472a74df
Jessica Sattler <jessicasattler28@gmail.com>
2016-10-31 15:08:29 -0500

    Phone number validation is now working, yay!

--- public/assets/js/signup-form.js
+++ public/assets/js/signup-form.js
@@ -1,4 +1,26 @@
 $().ready(function() {
+		// Override Defaults in jQuery plugin
+	$.validator.setDefaults({
+		highlight: function(element) {
+
+			$(element).closest('.form-group').addClass('has-error');
+
+		},
+		unhighlight: function(element) {
+
+			$(element).closest('.form-group').removeClass('has-error');
+
+		},
+		errorElement: 'span',
+		errorClass: 'help-block',
+		errorPlacement: function(error, element) {
+			if(element.parent('.input-group').length) {
+				error.insertAfter(element.parent());
+			} else {
+				error.insertAfter(element);
+			}
+		}
+	});
 	//validate signup form on keyup and submit
 	$("#signupForm").validate({
 		rules: {
@@ -11,7 +33,7 @@ $().ready(function() {
 			},
 			phone:{
 				required:true,
-				phone: true
+				phoneUS: true
 			},
 			password:{
 				required: true,
