83d49b36668b37ab4af74bfd40f44ab4ea4820a2
Mittsy Tidwell <mtidwell506@gmail.com>
2016-11-01 17:00:37 -0500

    navbar is smaller and less noticable

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,16 +1,5 @@
 <footer>
 	<div class="footer-bottom">
-		<div class="container">
-			<p class="pull-left"> Copyright © FoodNow 2016. All right reserved. </p>
-			<div class="pull-right">
-				<ul class="social">
-	                <li> <a href="#"> <i class=" fa fa-facebook">   </i> </a> </li>
-	                <li> <a href="#"> <i class="fa fa-twitter">   </i> </a> </li>
-	                <li> <a href="#"> <i class="fa fa-google-plus">   </i> </a> </li>
-	                <li> <a href="#"> <i class="fa fa-pinterest">   </i> </a> </li>
-	                <li> <a href="#"> <i class="fa fa-youtube">   </i> </a> </li>
-	            </ul>
-			</div>
-		</div>
+		<p> Copyright © FoodNow 2016. All right reserved. </p>
 	</div><!--/.footer-bottom--> 
 </footer> 
