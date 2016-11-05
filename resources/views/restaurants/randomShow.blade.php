39a4c1b196d2634086e17238d29998a5c198ec2a
Jessica Sattler <jessicasattler28@gmail.com>
2016-11-05 15:09:46 -0500

    There's no problem anymore with the random show blade getting undefied variables. I moved my modal script out of  the random.js and put jesssicaScripts.js back and that problem is solved. The navbar for the loging dropdown is still not working

--- resources/views/restaurants/randomShow.blade.php
+++ resources/views/restaurants/randomShow.blade.php
@@ -11,7 +11,8 @@
     <link rel="stylesheet" href="{{ URL::asset('assets/sweetalert-master/dist/sweetalert.css') }}" />
 
 	{{-- jQuery.js --}}
-	<script type="text/javascript" src="{{ URL::asset('assets/js/jQuery.js') }}"></script>
+	{{-- <script type="text/javascript" src="{{ URL::asset('assets/js/jQuery.js') }}"></script> --}}
+		<script type="text/javascript" src="{{ URL::asset('assets/js/jquery-1.11.1.min.js') }}"></script>
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
 
     {{-- Bootstrap.js --}}
@@ -184,5 +185,6 @@
 	{{-- Custom JS --}}
 	
 	<script type="text/javascript" src="{{ URL::asset('assets/js/random.js') }}"></script>
+	<script type="text/javascript" src="{{ URL::asset('assets/js/jessicaScripts.js') }}"></script>
 @stop
 
