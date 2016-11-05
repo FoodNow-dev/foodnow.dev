0a1ac02eebfccd2613b480777071cc29d3520198
Jessica Sattler <jessicasattler28@gmail.com>
2016-11-05 15:19:52 -0500

    Deleted the modal code from random js to clean it up

--- resources/views/restaurants/randomShow.blade.php
+++ resources/views/restaurants/randomShow.blade.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="{{ URL::asset('assets/sweetalert-master/dist/sweetalert.css') }}" />
 
 	{{-- jQuery.js --}}
-	{{-- <script type="text/javascript" src="{{ URL::asset('assets/js/jQuery.js') }}"></script> --}}
+	<script type="text/javascript" src="{{ URL::asset('assets/js/jQuery.js') }}"></script>
 		<script type="text/javascript" src="{{ URL::asset('assets/js/jquery-1.11.1.min.js') }}"></script>
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
 
