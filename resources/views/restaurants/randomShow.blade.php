5d9982db1b261e7183898f01cef3568e55439f74
Ben Roberts <mikebenroberts@gmail.com>
2016-11-04 21:49:11 -0500

    Fixed modal positioning on the random blade

--- resources/views/restaurants/randomShow.blade.php
+++ resources/views/restaurants/randomShow.blade.php
@@ -1,17 +1,33 @@
 @extends('layouts.master')
 
 @section('css')
+	{{-- Custom CSS --}}
 	<link rel="stylesheet" type="text/css" href="/assets/css/rest-show.css">
+
+	{{-- Chosen CSS --}}
 	<link rel="stylesheet" href="{{ URL::asset('assets/js/chosen_v1.6.2/chosen.min.css') }}"/>
+
+	{{-- Sweet Alerts CSS --}}
     <link rel="stylesheet" href="{{ URL::asset('assets/sweetalert-master/dist/sweetalert.css') }}" />
-	 <script type="text/javascript" src="{{ URL::asset('assets/js/jQuery.js') }}"></script>
+
+	{{-- jQuery.js --}}
+	<script type="text/javascript" src="{{ URL::asset('assets/js/jQuery.js') }}"></script>
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-     <script type="text/javascript" src="{{ URL::asset('assets/js/bootstrap.min.js') }}"></script>
-     <script type="text/javascript" src="{{ URL::asset('assets/js/jquery-validation-1.15.1/dist/jquery.validate.min.js') }}"></script>
-      <script type="text/javascript" src="{{ URL::asset('assets/js/jquery-validation-1.15.1/dist/additional-methods.js') }}"></script>
-     
-     <script type="text/javascript" src="{{ URL::asset('assets/js/chosen_v1.6.2/chosen.jquery.min.js') }}"></script>
-     <script type="text/javascript" src="{{ URL::asset('assets/sweetalert-master/dist/sweetalert.min.js') }}"></script>
+
+    {{-- Bootstrap.js --}}
+    <script type="text/javascript" src="{{ URL::asset('assets/js/bootstrap.min.js') }}"></script>
+    
+	{{-- jQuery Validation --}}
+    <script type="text/javascript" src="{{ URL::asset('assets/js/jquery-validation-1.15.1/dist/jquery.validate.min.js') }}"></script>
+
+    {{-- Additional Methods --}}
+    <script type="text/javascript" src="{{ URL::asset('assets/js/jquery-validation-1.15.1/dist/additional-methods.js') }}"></script>
+
+    {{-- Chosen.js --}}
+    <script type="text/javascript" src="{{ URL::asset('assets/js/chosen_v1.6.2/chosen.jquery.min.js') }}"></script>
+
+    {{-- Sweet Alerts.js --}}
+    <script type="text/javascript" src="{{ URL::asset('assets/sweetalert-master/dist/sweetalert.min.js') }}"></script>
 @stop
 
 @section('content')
@@ -23,13 +39,13 @@
 			<div class="modal-content">
 				<div class="modal-header">
 					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-					<h4 class="modal-title" id="exampleModalLabel">Create Event</h4>
+					<h3 class="modal-title" id="exampleModalLabel"><b>Create Event</b></h3>
 				</div>
 				<div class="modal-body">
 					{!! csrf_field() !!}
-					<div class="form-group">
+					<div class="form-group row">
 					{{-- dropdown --}}
-						<div class="col-xs-7 ">
+						<div class="col-xs-12">
 						 	<select class="my_select_box" data-placeholder="Select Friends" name="mytext[]" multiple>
 						 	  	@foreach($friends as $friend)
 						 	  		@if($user->id != $friend->id)
@@ -39,21 +55,14 @@
 							</select>
 						</div>
 						{{-- dynamic input field buttons --}}
-						<div class="input_fields_wrap ">
-							<button class="btn btn-default add_field_button">Add other numbers</button>
-
-							<div class="form-group">
-								<label class="sr-only" for="mytext[]"><button class="btn btn-default add_field_button">Add other numbers</button></label>
-								<div class="input-group">
-									{{-- fields to type phone numbers appears here --}}
-								</div>
+						<div class="input_fields_wrap col-xs-12">
+							<button class="btn-lg btn-primary add_field_button">Add other numbers</button>
+							<div class="form-group row">
+								{{-- fields to type phone numbers appears here --}}
 							</div>
     					</div>
-						<div class="form-group col-xs-8 col-xs-offset-7">
-						{{-- 	<textarea id="email_body" name="email_body" rows="4" cols="50" placeholder="">{{ Auth::user()->first_name }} {{ Auth::user()->last_name }} is inviting you to dinner at {{ $place['name']}}!</textarea> --}}
-
-						{{-- I temporarily added the following so I wouldn't get an error for not finding "places" --}}
-						<textarea id="email_body" name="email_body" rows="3" cols="25" placeholder="">We're meeting in Olive Garden at 7 pm</textarea>
+						<div class="form-group">
+							<textarea id="email_body" name="email_body" rows="4" cols="50" placeholder=""></textarea>
 						</div>
 					</div>{{-- /.form-group --}}
 			  	</div>{{-- /.modal-body --}}
@@ -66,30 +75,28 @@
 			</div>
 		</form>
 		<script>
-		// validation
+			// validation
 	    	$.validator.addMethod("cRequired", $.validator.methods.required, "Please provide a phone number");
 	    	$.validator.addMethod("cPhone", $.validator.methods.phoneUS, "Please provide a valid U.S. phone number");
 	    	$.validator.addClassRules("phone", {cRequired:true, cPhone:true});
 		    $( "#rest-info" ).validate({
-		  rules: {
-		    email_body: {
-		      required: true,
-		      minlength: 2
-		    }
-			  }
+			  	rules: {
+				    email_body: {
+				      	required: true,
+				      	minlength: 2
+				    }
+				}
 			});
-
-		</script>
-		<script>
-		//dropdown
+		
+			//dropdown
 			$(".my_select_box").chosen({
 				disable_search_threshold: 1,
 				no_results_text: "Oops, nothing found!",
 				width: "95%",
 				display_selected_options:false
 			});
-		</script>
-		<script>
+
+			// sweet alert
 		   $(".send").click(function() { 
 				swal("Message Sent", "Have a good time!", "success");
 			});
@@ -102,69 +109,68 @@
 
 
 <!-- Top content -->
-	<div class="top-content">
-		<div class="inner-bg">
+<div class="top-content">
+	<div class="inner-bg">
+		<div class="row">
+			<div class="fixed col-sm-7 col-sm-offset-1 text text-center show-box animated flipInX">
 				<div class="row">
-					<div class="fixed col-sm-7 col-sm-offset-1 text text-center show-box animated flipInX">
-					<div class="row">
-						{{-- <h1><strong>{{ $place['name'] }}</strong></h1> --}}
-						<div class="description">
-							{{-- Restaurant Info appends here --}}
-						</div>
+					<div class="description">
+						{{-- Restaurant Info appends here --}}
 					</div>
-					<!-- Slider -->
-					<div class="row-fluid">
-						<div class="span9" id="slider">
-							<!-- Top part of the slider -->
-							<div class="row-fluid">
-								<div class="span2" id="carousel-bounding-box">
-									<div id="myCarousel" class="carousel slide">
-										<!-- Carousel items -->
-										<div class="carousel-inner">
-											
-										</div>
-							  
+				</div>
+				<!-- Slider -->
+				<div class="row-fluid">
+					<div class="span9" id="slider">
+						<!-- Top part of the slider -->
+						<div class="row-fluid">
+							<div class="span2" id="carousel-bounding-box">
+								<div id="myCarousel" class="carousel slide">
+									<!-- Carousel items -->
+									<div class="carousel-inner">
+										{{-- Images append here --}}
 									</div>
-								  
-									<!-- Carousel nav -->
-									<div class="carousel-controls-mini">
-										<a href="#myCarousel" class="direction btn-link-1" data-slide="prev"><i class="fa fa-chevron-left" aria-hidden="true"></i></a>
-										<a href="#myCarousel" class="direction btn-link-1" data-slide="next"><i class="fa fa-chevron-right" aria-hidden="true"></i></a>
-									</div>
+								</div>
+								<!-- Carousel nav -->
+								<div class="carousel-controls-mini">
+									<a href="#myCarousel" class="direction btn-link-1" data-slide="prev"><i class="fa fa-chevron-left" aria-hidden="true"></i></a>
+									<a href="#myCarousel" class="direction btn-link-1" data-slide="next"><i class="fa fa-chevron-right" aria-hidden="true"></i></a>
 								</div>
 							</div>
 						</div>
 					</div>
-					<br>
-					<br>
-					<!--/Slider-->
+				</div>
+				<br>
+				<br>
+				<!--/Slider-->
 				<div class="col-sm-6 col-sm-offset-3">
-					<button type="button" class="btn-create" data-toggle="modal" data-target="#modal" href="#">Create Event</button>
+					<button type="button" class="btn-create" data-toggle="modal" data-target="#modal" href="#"><b>Create Event</b></button>
 				</div>
-					
-				</div>
-				<div class="col-sm-5 col-sm-offset-6 form-box">
-					<div id="map">
-						{{-- Map Renders Here --}}
-					</div>
+			</div>
+			<div class="col-sm-5 col-sm-offset-6 form-box">
+				<div id="map">
+					{{-- Map Renders Here --}}
 				</div>
 			</div>
 		</div>
 	</div>
+</div>
+
 				
 
 @stop
 
 
 @section('js-script')
-
+	<script type="text/javascript">
+		var user = '{{ Auth::user()->first_name }}';
+	</script>
 
 	{{-- Google Maps API --}}
 	{{-- MAIN API --}}
-	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC7khJALOM8uuLkCAdi4lsDQFbojqEulHs&libraries=places&callback=initMap" async defer></script>
+	{{-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC7khJALOM8uuLkCAdi4lsDQFbojqEulHs&libraries=places&callback=initMap" async defer></script> --}}
 
 	{{-- JESSICA API --}}
-	{{-- // <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBZU6dw9xUbnO_HXZ07ASIHhMkMHUeqpI4&libraries=places&callback=initMap" async defer></script> --}}
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBZU6dw9xUbnO_HXZ07ASIHhMkMHUeqpI4&libraries=places&callback=initMap" async defer></script>
 	
 	
 	{{-- BENS API --}}
@@ -178,5 +184,4 @@
 	{{-- Custom JS --}}
 	<script type="text/javascript" src="{{ URL::asset('assets/js/random.js') }}"></script>
 @stop
-
 
